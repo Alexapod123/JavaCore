9fffe8a3c30b3dea9e5640224549eecfc582aaa6
Alexapod123 <alex_krim99@mail.ru>
2024-10-24 14:47:26 +0300

    t3

--- src/dz_2/Task_3.java
+++ src/dz_2/Task_3.java
@@ -20,7 +20,7 @@ public class Task_3 {
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] == 0 && arr[i + 1] == 0) {
                 flag = true;
-                    break;
+                break;
             }
         }
         return flag;
