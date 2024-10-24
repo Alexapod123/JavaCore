3241f799b8780a9fb90ecb17102759c29b7c74a6
Alexapod123 <alex_krim99@mail.ru>
2024-10-24 14:05:32 +0300

    t1

---
+++ src/dz_2/Task_1.java
@@ -0,0 +1,31 @@
+package dz_2;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        int length = 10;
+        int[] array = new int [length];
+        generateArray(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println(countEvens(array));
+    }
+
+    public static int[] generateArray(int[] arr){
+        Random rand = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+        }
+        return arr;
+    }
+    public static int countEvens(int[] arr){
+        int count =0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]%2==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
