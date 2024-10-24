97151427b4156c35b13391dab02b2c80f7d903b2
Alexapod123 <alex_krim99@mail.ru>
2024-10-24 14:06:35 +0300

    t1

---
+++ src/dz_2/Task2.java
@@ -0,0 +1,7 @@
+package dz_2;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/dz_2/Task_1.java
+++ src/dz_2/Task_1.java
@@ -1,3 +1,8 @@
+/*
+Написать метод, возвращающий количество чётных элементов массива.
+countEvens([2, 1, 2, 3, 4]) → 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3, 5]) → 0
+ */
+
 package dz_2;
 
 import java.util.Arrays;
